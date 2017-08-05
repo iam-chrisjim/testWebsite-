96052ec7cb87b5c306795d326d5ddc8cce021dc2
iam-chrisjim <chrisjimphotos@gmail.com>
2017-08-05 17:04:37 +0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* smooth scroll */
+$(document).ready(function(){ 
+	$(".scroll").click(function(event){
+		event.preventDefault();
+		$("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
+		$('.navbar-default a').removeClass('selected');
+		$(this).addClass('selected');
+    	});
+});
+
+$(document).on('click.bs.collapse.data-api touchstart.bs.collapse.data-api', '[data-toggle="collapse"]', function (e) { 
+});
+
+
+
+
